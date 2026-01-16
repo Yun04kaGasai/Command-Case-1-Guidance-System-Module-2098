cb0f4c452328d45a6919735af51e4ef8e3ee4c40
Kare0 Sigma <hacatrank06@gmail.com>
2026-01-16 08:21:56 +0300

    Code v2
    Добавлен код для платы STM32 , он пришёл на замену Arduino Nano, сделали багфиксы и отредактировали модели (цвет ещё не изменён, но вскоре будет изменён на цвет реального прототипа)
    
    Co-Authored-By: GoptarEA <119011269+GoptarEA@users.noreply.github.com>
    Co-Authored-By: Nadi <96995868+Nadi-Lu@users.noreply.github.com>
    Co-Authored-By: sonka <254969097+sofiazot@users.noreply.github.com>
    Co-Authored-By: kizar7 <255301084+kizar7@users.noreply.github.com>
    Co-Authored-By: NiKEKlay <184669630+NikolayKek@users.noreply.github.com>

--- Assets/raspberry_pi/Raspberry_Pi.ino
+++ Assets/raspberry_pi/Raspberry_Pi.ino
@@ -3,53 +3,61 @@
 #include <RF24.h>
 #include <Servo.h>
 
-#define LASER_PIN   5    // KY-008
-#define SERVO_PIN   6    // GH-S37A (азимут)
-#define CE_PIN      14   // nRF24
-#define CSN_PIN     15
+#define LASER_PIN    5
+#define SERVO_AZIM   6
+#define SERVO_TILT   7
+#define SERVO_COVER  8
+#define CE_PIN  14
+#define CSN_PIN 15
 
 RF24 radio(CE_PIN, CSN_PIN);
-Servo servo;
+Servo servoAzim, servoTilt, servoCover;
 
 const byte address[6] = "TGT01";
 
 struct Command {
-  bool laser_on = false;
-  uint8_t servo_angle = 90;
+  bool laserOn;
+  int8_t azimuth;
+  int8_t tilt;
 };
+
+int angleToServo(int8_t angle) {
+  return 90 + angle;
+}
 
 void setup() {
   pinMode(LASER_PIN, OUTPUT);
   digitalWrite(LASER_PIN, LOW);
-  
-  servo.attach(SERVO_PIN);
-  servo.write(90);
-
+  servoAzim.attach(SERVO_AZIM);
+  servoTilt.attach(SERVO_TILT);
+  servoCover.attach(SERVO_COVER);
+  servoCover.write(0);
+  servoAzim.write(90);
+  servoTilt.write(90);
   SPI.begin();
-
   radio.begin();
   radio.setPALevel(RF24_PA_MIN);
   radio.setDataRate(RF24_1MBPS);
   radio.openReadingPipe(0, address);
   radio.startListening();
-
   Serial.begin(115200);
-  Serial.println("Pico: готов");
 }
 
 void loop() {
   if (radio.available()) {
     Command cmd;
     radio.read(&cmd, sizeof(cmd));
-
-    digitalWrite(LASER_PIN, cmd.laser_on ? HIGH : LOW);
-    servo.write(cmd.servo_angle);
-
-    Serial.print("Лазер: ");
-    Serial.print(cmd.laser_on ? "ON" : "OFF");
-    Serial.print(" | Угол: ");
-    Serial.print(cmd.servo_angle);
-    Serial.println("°");
+    static bool coverOpened = false;
+    if (!coverOpened && (cmd.azimuth != 0 || cmd.tilt != 0)) {
+      servoCover.write(90);
+      delay(1000);
+      coverOpened = true;
+    }
+    int az = angleToServo(cmd.azimuth);
+    int ti = angleToServo(cmd.tilt);
+    servoAzim.write(az);
+    servoTilt.write(ti);
+    digitalWrite(LASER_PIN, cmd.laserOn ? HIGH : LOW);
   }
   delay(10);
 }
