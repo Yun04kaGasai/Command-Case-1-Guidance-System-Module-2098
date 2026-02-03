5dde358175e419b12990b070cf461a3f45247553
Kare0 Sigma <hacatrank06@gmail.com>
2026-02-03 14:01:34 +0300

    Обновление кода

--- Программный код/stm32.ino
+++ Программный код/stm32.ino
@@ -6,62 +6,85 @@
 #define CSN_PIN PA1
 
 RF24 radio(CE_PIN, CSN_PIN);
-const byte address[6] = "TGT01";
+const byte pipe[6] = "TGT01";
 
-struct Command {
-  bool laserOn;
+struct Message {
+  uint8_t sender_id;
+  uint8_t target_id;
+  char command[4];
   int8_t azimuth;
   int8_t tilt;
+  uint8_t mode;
 };
 
+const char* modeToString(uint8_t mode) {
+  switch (mode) {
+    case 1: return "Horizontal";
+    case 2: return "Vertical";
+    case 3: return "Diagonal1";
+    case 4: return "Diagonal2";
+    default: return "Idle";
+  }
+}
+
 void setup() {
+  pinMode(PC13, OUTPUT);
   Serial.begin(115200);
   SPI.setMOSI(PA7);
   SPI.setMISO(PA6);
-  SPI.setSCK(PA5);
+  SPI.setSCLK(PA5);
   SPI.begin();
-
   radio.begin();
   radio.setPALevel(RF24_PA_MIN);
   radio.setDataRate(RF24_1MBPS);
-  radio.openWritingPipe(address);
+  radio.openWritingPipe(pipe);
+  radio.openReadingPipe(1, pipe);
   radio.stopListening();
+  digitalWrite(PC13, LOW);
 }
 
-void sendPosition(bool laser, int8_t az, int8_t ti) {
-  Command cmd = {laser, az, ti};
-  radio.write(&cmd, sizeof(cmd));
-  delay(100);
-}
-
-void runScans() {
-  sendPosition(false, 0, 0);
-  delay(1000);
-  for (int a = -40; a <= 40; a += 10) {
-    sendPosition(true, a, 0);
-    delay(3000);
+void sendScanCommand() {
+  Message msg;
+  msg.sender_id = 67;
+  msg.target_id = 69;
+  strncpy(msg.command, "SCAN", 4);
+  msg.command[3] = '\0';
+  msg.azimuth = 0;
+  msg.tilt = 0;
+  msg.mode = 0;
+  bool ok = radio.write(&msg, sizeof(msg));
+  if (ok) {
+    Serial.println("команда SCAN отправлена");
+  } else {
+    Serial.println("ошибка отправки SCAN");
   }
-  for (int t = -40; t <= 40; t += 10) {
-    sendPosition(true, 0, t);
-    delay(3000);
-  }
-  for (int i = -40; i <= 40; i += 10) {
-    sendPosition(true, i, i);
-    delay(3000);
-  }
-  for (int i = -40; i <= 40; i += 10) {
-    sendPosition(true, i, -i);
-    delay(3000);
-  }
-  sendPosition(false, 0, 0);
 }
 
 void loop() {
   if (Serial.available()) {
     String input = Serial.readStringUntil('\n');
-    if (input.indexOf("START") >= 0) {
-      runScans();
+    input.trim();
+    if (input == "START") {
+      sendScanCommand();
+      delay(100);
+      radio.startListening();
     }
   }
+
+  if (radio.available()) {
+    Message msg;
+    radio.read(&msg, sizeof(msg));
+
+    if (msg.sender_id == 69 && strncmp(msg.command, "POS", 3) == 0) {
+      Serial.print("От Pico(#69):");
+      Serial.print("азимут=");
+      Serial.print(msg.azimuth);
+      Serial.print("°,наклон=");
+      Serial.print(msg.tilt);
+      Serial.print("°,режим=");
+      Serial.println(modeToString(msg.mode));
+    }
+  }
+
   delay(10);
 }
